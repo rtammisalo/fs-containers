53f1fa7832f8f314737d385f128398e35cf22054
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-12-12 19:09:20 +0200

    Add Ex12.7: Fix GET by id, PUT

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,56 @@
-const express = require('express');
+const express = require('express')
 const { Todo } = require('../mongo')
-const router = express.Router();
+const router = express.Router()
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  res.send(todo)
+})
 
-const singleRouter = express.Router();
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
-
-  next()
+  try {
+    req.todo = await Todo.findById(id)
+    if (!req.todo) return res.sendStatus(404)
+    next()
+  } catch (error) {
+    return res.sendStatus(400)
+  }
 }
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
-/* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  res.status(200).json(req.todo)
+})
 
-/* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  if ('text' in req.body) req.todo.text = req.body.text
+  if ('done' in req.body) req.todo.done = req.body.done
+  try {
+    await req.todo.save()
+    res.sendStatus(200)
+  } catch (error) {
+    res.sendStatus(400)
+  }
+})
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
